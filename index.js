7946d15a4e4b11739739748c55edea21258f89d2
chandan-singh02 <chandanxingh173@gmail.com>
2022-03-14 18:46:26 +0530

    changed h2 element & button border-radius

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-const hex =[0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
-btn.addEventListener("click",function(){
-    let hexcolor = "#";
-    for(let i=0;i<6;i++){
-        hexcolor += hex[getrandomnumber()];
+btn.addEventListener("click", function () {
+  let hexcolor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexcolor += hex[getrandomnumber()];
+  }
 
-    }
-   
-    color.textContent = hexcolor;
-    //here the color code is changed
-    document.body.style.backgroundColor = hexcolor;
+  color.textContent = hexcolor;
+  //here the color code is changed
+  document.body.style.backgroundColor = hexcolor;
 });
-function getrandomnumber(){
-    //random function will generate all random number in decimal form 
-    //and that decimal number is being multiplied by hex values , and after
-    // that they get floor (low ) values and we get our random color code .
-    return Math.floor(Math.random()*hex.length);
+function getrandomnumber() {
+  //random function will generate all random number in decimal form
+  //and that decimal number is being multiplied by hex values , and after
+  // that they get floor (low ) values and we get our random color code .
+  return Math.floor(Math.random() * hex.length);
 }
